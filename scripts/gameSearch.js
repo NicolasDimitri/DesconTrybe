f12600e02c1e99ba1369894abe3173feb4ea87e6
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-09-28 18:28:49 -0300

    Corrigindo tamanho da thumb

--- scripts/gameSearch.js
+++ scripts/gameSearch.js
@@ -7,14 +7,11 @@ function parse_query_string(query) {
     var pair = vars[i].split("=");
     var key = decodeURIComponent(pair[0]);
     var value = decodeURIComponent(pair[1]);
-    // If first entry with this name
     if (typeof query_string[key] === "undefined") {
       query_string[key] = decodeURIComponent(value);
-      // If second entry with this name
     } else if (typeof query_string[key] === "string") {
       var arr = [query_string[key], decodeURIComponent(value)];
       query_string[key] = arr;
-      // If third or later entry with this name
     } else {
       query_string[key].push(decodeURIComponent(value));
     }
@@ -23,28 +20,48 @@ function parse_query_string(query) {
 }
 
 async function fetchGameByName(name) {
-  const response = await fetch(ENDPOINT + `games?title=${name}`);
+  const response = await fetch(ENDPOINT + `games?title=${name}&limit=20`);
   const json = await response.json();
   return json;
 };
 
 async function fetchGameByID(id) {
-  const response = await fetch(ENDPOINT + `games?title=${id}`);
+  const response = await fetch(ENDPOINT + `games?id=${id}`);
   const json = await response.json();
   return json;
 };
 
+function createCustomElement(tag, className, text='') {
+  const element = document.createElement(tag);
+  element.classList.add(className);
+  element.innerText = text;
+  return element;
+}
+
+function createGameElement({external:title, thumb}) {
+  const gameSection = createCustomElement('section', 'game-section');
+
+  const gameThumb = createCustomElement('div', 'game-thumb');
+  gameThumb.style.backgroundImage = `url(${thumb})`;
+  gameSection.appendChild(gameThumb);
+
+  const gameTitle = createCustomElement('p', 'game-title', title);
+  gameSection.appendChild(gameTitle);
+
+  return gameSection;
+};
+
 async function insertGames(games) {
   games.forEach((game) => {
-    // const gameElement = document.createElement('li');
-    console.log(game.gameID);
-
+    const gameElement = createGameElement(game);
+    document.body.appendChild(gameElement);
   });
-}
+};
 
 window.onload = async () => {
   const query = window.location.search.substring(1);
   const {title} = parse_query_string(query);
-  const games = await fetchGameByName(title)
+  const games = await fetchGameByName(title);
+  console.log(games)
   insertGames(games);
 };
