016fd66a6eedd4d667a9539343e7ad5d8b1f3bb8
NicolasDimitri <nicolasdodimitri@gmail.com>
2021-09-28 22:19:09 -0300

    preço

--- scripts/gameSearch.js
+++ scripts/gameSearch.js
@@ -1,15 +1,15 @@
-const ENDPOINT = 'https://www.cheapshark.com/api/1.0/'
+const ENDPOINT = 'https://www.cheapshark.com/api/1.0/';
 
 function parse_query_string(query) {
-  var vars = query.split("&");
+  var vars = query.split('&');
   var query_string = {};
   for (var i = 0; i < vars.length; i++) {
-    var pair = vars[i].split("=");
+    var pair = vars[i].split('=');
     var key = decodeURIComponent(pair[0]);
     var value = decodeURIComponent(pair[1]);
-    if (typeof query_string[key] === "undefined") {
+    if (typeof query_string[key] === 'undefined') {
       query_string[key] = decodeURIComponent(value);
-    } else if (typeof query_string[key] === "string") {
+    } else if (typeof query_string[key] === 'string') {
       var arr = [query_string[key], decodeURIComponent(value)];
       query_string[key] = arr;
     } else {
@@ -23,22 +23,22 @@ async function fetchGameByName(name) {
   const response = await fetch(ENDPOINT + `games?title=${name}&limit=20`);
   const json = await response.json();
   return json;
-};
+}
 
 async function fetchGameByID(id) {
   const response = await fetch(ENDPOINT + `games?id=${id}`);
   const json = await response.json();
   return json;
-};
+}
 
-function createCustomElement(tag, className, text='') {
+function createCustomElement(tag, className, text = '') {
   const element = document.createElement(tag);
   element.classList.add(className);
   element.innerText = text;
   return element;
 }
 
-function createGameElement({external:title, thumb}) {
+function createGameElement({ external: title, thumb, cheapest }) {
   const gameSection = createCustomElement('section', 'game-section');
 
   const gameThumb = createCustomElement('div', 'game-thumb');
@@ -48,8 +48,11 @@ function createGameElement({external:title, thumb}) {
   const gameTitle = createCustomElement('p', 'game-title', title);
   gameSection.appendChild(gameTitle);
 
+  const gamePrice = createCustomElement('p', 'game-price', cheapest);
+  gameSection.appendChild(gamePrice);
+
   return gameSection;
-};
+}
 
 async function insertGames(games) {
   const main = document.querySelector('main');
@@ -57,12 +60,12 @@ async function insertGames(games) {
     const gameElement = createGameElement(game);
     main.appendChild(gameElement);
   });
-};
+}
 
 window.onload = async () => {
   const query = window.location.search.substring(1);
-  const {title} = parse_query_string(query);
+  const { title } = parse_query_string(query);
   const games = await fetchGameByName(title);
-  console.log(games)
+  console.log(games);
   insertGames(games);
 };
