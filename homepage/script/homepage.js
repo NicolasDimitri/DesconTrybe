81e99162b9a5dc9dfa0bd250f7a1a280475c6a12
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-10-01 16:40:16 -0300

    Refatorando scripts

--- homepage/script/homepage.js
+++ homepage/script/homepage.js
@@ -1,60 +1,3 @@
-let stores;
-let deals;
-const urlDeals = 'https://www.cheapshark.com/api/1.0/deals?pageSize=50';
-const urlStores = 'https://www.cheapshark.com/api/1.0/stores';
-
-async function fetchStores() {
-  const response = await fetch(urlStores);
-  const result = await response.json();
-  return result;
-}
-
-async function fetchDeals() {
-  const response = await fetch(urlDeals);
-  const result = await response.json();
-  return result;
-}
-
-function getStoreImage(id) {
-  const store = stores.find(({ storeID }) => id === storeID);
-  const logo = `https://www.cheapshark.com/${store.images.logo}`;
-  return logo;
-}
-
-function groupByDeals(deals) {
-  const groupedDeals = [];
-
-  for (let i = 0; i < deals.length; i++) {
-    const deal = deals[i];
-    const isRepeated = groupedDeals.some((game) => game.id === deal.gameID);
-
-    let game = {};
-    game.deals = [];
-
-    if (isRepeated) {
-      game = groupedDeals.find(({ id }) => deal.gameID === id);
-      game.deals.push(deal);
-      game.deals.sort((a, b) => a.salePrice - b.salePrice);
-    } else {
-      game.id = deal.gameID;
-      game.deals = [deal];
-      groupedDeals.push(game);
-    }
-  }
-  return groupedDeals;
-}
-
-function getGamesWithStores(gameList) {
-  const groupedDeals = groupByDeals(gameList);
-  const filteredGames = [];
-  groupedDeals.forEach((game) => {
-    const mainDeal = game.deals[0];
-    const ids = game.deals.map((deal) => deal.storeID);
-    mainDeal.storeIDS = ids;
-    filteredGames.push(mainDeal);
-  });
-  return filteredGames;
-}
 
 async function insertDeals(data) {
   const filteredGames = getGamesWithStores(data);
@@ -70,22 +13,6 @@ async function insertDeals(data) {
       });
     }
   );
-}
-
-function createLogosIcons(storeIDS) {
-  const sectionLogo = document.createElement('section');
-  sectionLogo.classList.add('game-stores');
-
-  storeIDS.forEach((id) => {
-    const image = getStoreImage(id);
-    const imgLogo = document.createElement('img');
-    imgLogo.classList.add('logo');
-    imgLogo.src = image;
-    sectionLogo.appendChild(imgLogo);
-  });
-
-  sectionLogo.firstChild.classList.add('selected');
-  return sectionLogo;
 }
 
 function createElements({
